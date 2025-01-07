7245526c5109d921336c1ca9d238055657f076db
veritons <veritons@younivers.fr>
2025-01-07 15:00:39 +0100

    fini components.test

--- src/tests/component.test.js
+++ src/tests/component.test.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import Link from '../Link'; 
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { describe } from 'jest-circus';
-import '@testing-library/jest-dom';
+import React from "react";
+import Link from "../Link";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe } from "jest-circus";
+import "@testing-library/jest-dom";
 
 // utiliser le composant Link comme celui-ci dans vos tests : <Link page="http://www.facebook.com">Facebook</Link>
-describe('Link component', () => { 
-    it('renders with correct text content', () => {
-        // vérifie que le composant Link est bien rendu avec le texte "Facebook"
-    });
+describe("Link component", () => {
+  it("renders with correct text content", () => {
+    render(<Link page="http://www.facebook.com">Facebook</Link>);
+    expect(screen.getByText("Facebook")).toHaveTextContent("Facebook");
+  });
 
-    
-    it('has the correct default classname', () => {
-        // vérifie que le composant Link a la classe "normal" lorsqu'il est rendu
-    });
-    
-    it('renders correct href attribute', () => {
-        // vérifie que le lien href est bien "http://www.facebook.com"
+  it("has the correct default classname", () => {
+    render(<Link page="http://www.facebook.com">Facebook</Link>);
+    expect(screen.getByText("Facebook")).toHaveClass("normal");
+  });
 
-    });
-    
-    it('changes classname on hover', () => {
-        // vérifie que la classe "hovered" est bien ajoutée lors du survol sur le composant et retirée lors du unhover
+  it("renders correct href attribute", () => {
+    render(<Link page="http://www.facebook.com">Facebook</Link>);
+    expect(screen.getByText("Facebook")).toHaveAttribute(
+      "href",
+      "http://www.facebook.com"
+    );
+  });
 
-    });
+  it("changes classname on hover", async () => {
+    render(<Link page="http://www.facebook.com">Facebook</Link>);
+    const element = screen.getByText("Facebook");
+    await userEvent.hover(element);
+    expect(element).toHaveClass("hovered");
+  });
 });
