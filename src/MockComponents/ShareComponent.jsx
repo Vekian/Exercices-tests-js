d32bfc5f4efc085b9f1b113e818b5571efb09e53
veritons <veritons@younivers.fr>
2025-01-07 15:41:08 +0100

    fini mockComponent.js

--- src/MockComponents/ShareComponent.jsx
+++ src/MockComponents/ShareComponent.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import InstallComponent  from './InstallComponent'
-import UserComponent from './UserComponent'
+import React from "react";
+import InstallComponent from "./InstallComponent";
+import UserComponent from "./UserComponent";
 
 export default function ShareComponent() {
-    return (
-        <div>
-            <InstallComponent para1='title1'/>
-            <UserComponent para2='title2' />
-        </div>
-    )
+  return (
+    <div data-testid="share-component">
+      <InstallComponent para1="title1" />
+      <UserComponent para2="title2" />
+    </div>
+  );
 }
--- src/tests/mockComponent.test.js
+++ src/tests/mockComponent.test.js
@@ -1,16 +1,39 @@
-import ShareComponent from '../MockComponents/ShareComponent';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import React from 'react';
+import ShareComponent from "../MockComponents/ShareComponent";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
 
 //mocker le composant UserComponent
+jest.mock("../MockComponents/UserComponent", () => {
+  return {
+    __esModule: true,
+    default: ({ para2 }) => (
+      <div>
+        <h1>{para2}</h1>
+      </div>
+    ),
+  };
+});
 //mocker le composant InstallComponent
-
+jest.mock("../MockComponents/InstallComponent", () => {
+  return {
+    __esModule: true,
+    default: ({ para1 }) => (
+      <div>
+        <h1>{para1}</h1>
+      </div>
+    ),
+  };
+});
 
 describe("ShareComponent", () => {
   it("should return correct component", () => {
+    render(<ShareComponent para1="title1" para2="title2" />);
     // vérifie que le composant ShareComponent est bien rendu
-
+    expect(screen.getByTestId("share-component")).toBeInTheDocument();
     // vérifie que les composants mockés contiennent le texte choisi dans le mock
+    expect(screen.getByText("title2")).toBeInTheDocument(); // pour UserComponent
+
+    expect(screen.getByText("title1")).toBeInTheDocument(); // pour InstallComponent
   });
 });
